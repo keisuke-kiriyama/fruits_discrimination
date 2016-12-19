e1835bb6b448d6f042a342ead7d1ce3fc819d3bf
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2016-12-19 23:36:17 +0900

    create model

--- input_image_data.py
+++ input_image_data.py
@@ -7,7 +7,10 @@ import os
 import tensorflow as tf
 
 NUM_CLASS = 5
-IMAGE_SIZE = 112
+IMAGE_SIZE = 28
+INPUT_SIZE = 20
+DST_INPUT_SIZE = 28
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 500
 
 FOOD_NAMES = {
     0: 'banana',
@@ -17,7 +20,10 @@ FOOD_NAMES = {
     4: 'peer'
 }
 
-EXTS = ['.JPG', '.JPEG', '.PNG', '.BMP']
+EXTS = ['.JPG', '.JPEG', '.PNG']
+
+def load_data_for_test(csv, batch_size):
+    return load_data(csv, batch_size, shuffle = False, distored = False)
 
 def load_data(csv, batch_size, shuffle = True, distored = True):
     queue = tf.train.string_input_producer(csv, shuffle=shuffle)
@@ -26,7 +32,6 @@ def load_data(csv, batch_size, shuffle = True, distored = True):
     filename, label = tf.decode_csv(value, [["path"], [1]], field_delim = " ")
     label = tf.cast(label, tf.int64)
     label = tf.one_hot(label, depth = NUM_CLASS, on_value = 1.0, off_value = 0.0, axis = -1)
-
     (imgpath, ext) = os.path.splitext(filename)
     n = EXTS.index(ext)
 
@@ -40,8 +45,55 @@ def load_data(csv, batch_size, shuffle = True, distored = True):
         image = tf.image.decode_png(png, channels=3)
         image = tf.cast(image, tf.float32)
         image.set_shape([IMAGE_SIZE, IMAGE_SIZE, 3])
-    elif n == 3:
-        pass
+
+    if distored:
+        cropsize = random.randint(INPUT_SIZE, INPUT_SIZE + (IMAGE_SIZE - INPUT_SIZE) / 2)
+        framesize = INPUT_SIZE + (cropsize - INPUT_SIZE) * 2
+        image = tf.image.resize_image_with_crop_or_pad(image, framesize, framesize)
+        image = tf.random_crop(image, [cropsize, cropsize, 3])
+        image = tf.image.random_flip_left_right(image)
+        image = tf.image.random_brightness(image, max_delta=0.8)
+        image = tf.image.random_contrast(image, lower=0.8, upper=1.0)
+        image = tf.image.random_hue(image, max_delta=0.04)
+        image = tf.image.random_saturation(image, lower=0.6, upper=1.4)
+
+    image = tf.image.resize_images(image, DST_INPUT_SIZE, DST_INPUT_SIZE)
+    image = tf.image.per_image_whitening(image)
+
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
+
+    return _generate_image_and_label_batch(
+        image,
+        label,
+        filename,
+        min_queue_examples, batch_size,
+        shuffle=shuffle)
+
+def _generate_image_and_label_batch(image, label, filename, min_queue_examples, batch_size, shuffle):
+    num_preprocess_threads = 16
+    capacity = min_queue_examples + 3 * batch_size
+
+    if shuffle:
+        images, label_batch, filename = tf.train.shuffle_batch(
+            [image, label, filename],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=capacity,
+            min_after_dequeue=min_queue_examples)
+    else:
+        images, label_batch, filename = tf.train.batch(
+            [image, label, filename],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_size)
+
+    # Display the training images in the visualizer.
+    tf.image_summary('image', images, max_images=100)
+
+    labels = tf.reshape(label_batch, [batch_size, NUM_CLASS])
+    return images, labels, filename
+
 
 
 
---
+++ main_fruits_discrimination.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+import sys
+import cv2
+import numpy as np
+import tensorflow as tf
+import tensorflow.python.pywrap_tensorflow
+from datetime import datetime
+import random
+import time
+import os
+
+import input_image_data
