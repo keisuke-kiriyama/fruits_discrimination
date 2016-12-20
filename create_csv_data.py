37d9920448414bf206150bcb9665bc7fd5a1e1e5
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2016-12-20 14:41:50 +0900

    modify create_csv program

--- create_csv_data.py
+++ create_csv_data.py
@@ -2,54 +2,54 @@
 import sys
 import os
 import csv
+from random import shuffle
 
 names = {
-        "banana_resize": 0,
-        "corn_resize": 1,
-        "greengrape_resize": 2,
-        "lemon_resize": 3,
-        "peer_resize": 4
-    }
+    "banana": 0,
+    "corn": 1,
+    "greengrape": 2,
+    "lemon": 3,
+    "peer": 4
+}
 
 exts = ['.JPG', '.JPEG', '.PNG', '.BMP', '.GIF']
 
 
-
-def dir_label(imgdir):
-    for dirpath, dirnames, filenames in os.walk(imgdir):
+def search_dir(imgdir):
+    dir_members = []
+    for dirpath, dirnames, _ in os.walk(imgdir):
         for dirname in dirnames:
-            if dirname in names:
-                n = names[dirname]
-                member_dir = os.path.join(dirpath, dirname)
-                create_csv(member_dir, n, dirname)
+            dir_member = os.path.join(dirpath, dirname)
+            dir_members.append(dir_member)
+    return dir_members
 
-def create_csv(member_dir, n, dirname):
-    for bottom_dirpath, bottom_dirnames, imgnames in os.walk(member_dir):
-        if not bottom_dirpath.endswith(dirname):
-            continue
-        for imgname in imgnames:
-            (img, ext) = os.path.splitext(imgname)
-            if ext.upper() in exts:
-                list_data = []
-                img_path = os.path.join(bottom_dirpath, imgname)
-                list_data.append(img_path)
-                list_data.append(n)
-                csv_writer.writerow(list_data)
+
+def create_csv(dir_members, basename, num_of_train_image):
+    with open(basename + '_test.csv', 'w') as test_f:
+        with open(basename + '_train.csv', 'w') as train_f:
+            csv_test_writer = csv.writer(test_f)
+            csv_train_writer = csv.writer(train_f)
+            for dir_member in dir_members:
+                file_list = os.listdir(dir_member)
+                shuffle(file_list)
+                category = dir_member.split('/')[-1]
+                label = names[category]
+                for n, img_name in enumerate(file_list):
+                    (img, ext) = os.path.splitext(img_name)
+                    img_path = os.path.join(dir_member, img_name)
+                    if ext.upper() not in exts:
+                        continue
+                    if n < num_of_train_image:
+                        csv_train_writer.writerow([img_path, label])
+                    else:
+                        csv_test_writer.writerow([img_path, label])
+
 
 if __name__ == '__main__':
     imgdir = sys.argv[1]
+    num_of_train_image = int(sys.argv[2])
     if not os.path.isdir(imgdir):
         sys.exit('%s is not directory' % imgdir)
 
-    with open('tes.csv', 'w') as f:
-        csv_writer = csv.writer(f)
-        dir_label(imgdir)
-
-
-
-    f.close()
-
-
-
-
-
+    dir_members = search_dir(imgdir)
+    create_csv(dir_members, 'fruits_image', num_of_train_image)
